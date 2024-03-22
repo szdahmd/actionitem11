205c21334ce67e6cd329395e02e86bead908b022
EC2 Default User <ec2-user@ip-172-31-30-197.ec2.internal>
2024-03-22 18:53:38 +0000

    got rid of one instance

--- main.tf
+++ main.tf
@@ -11,17 +11,3 @@ resource "aws_instance" "Red-Hat-Server" {
     Name = "RHEL-Server"
   }
 }
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "Ubuntu-Server" {
-  count         = 1
-  ami           = "ami-080e1f13689e07408"
-  instance_type = "t2.medium"
-  key_name      = "SazidAkeypair"
-   tags = {
-    Name = "RHEL-Server"
-  }
-}
